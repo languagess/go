ee79682d51187d98ff846f993ab28085defbe408
Ruchit Patel <ruchittpatell@gmail.com>
2024-10-27 22:44:52 -0700

    basic

---
+++ tour/main/tour.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+		"fmt"
+		"tour"
+)
+
+func main () {
+	fmt.Println("* * Types * *")
+	tour.Types()
+	fmt.Println()
+
+	fmt.Println("* * Loops * *")
+	tour.ForLoop()
+	fmt.Println()
+}
